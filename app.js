ca2baa417eb03075e60dd495afb195581a1b2061
delaemon <takeshi.music.5@gmail.com>
2015-12-11 23:57:40 +0900

    Add memcached

--- app.js
+++ app.js
@@ -3,8 +3,10 @@ const route = require("koa-route")
 const serve = require("koa-static")
 const views = require("koa-views")
 const logger = require('koa-logger')
+const co = require("co")
 const mysql = require("mysql-co")
 const gzip = require('koa-gzip');
+const memcached = require('node_memcached')
 const env = process.env
 
 app.use(logger())
@@ -21,7 +23,7 @@ config.db = {
     host: env.MYSQL_HOST || "localhost",
     port: env.MYSQL_PORT || "3306",
     user: env.MYSQL_USER || "root",
-    password: env.MYSQL_PASS || "",
+    password: env.MYSQL_PASS || "test",
     database: env.MYSQL_DATABASE || "matching"
 }
 
@@ -42,12 +44,17 @@ function wait(ms) {
   }
 }
 
+const options = options || {};
+const memcache_client = memcached.createClient(options.port || "11211", options.host || "localhost", options)
 app.use(
     route.get('/', function *index(){
         const sql = 'SELECT * FROM question WHERE id = ?'
         const res = yield this.db.query(sql, 1)
         const data = JSON.parse(res[0][0].data)
         //yield wait(3000)
+        memcache_client.get('key1',function(err, res){
+            console.log(res)
+        })
         yield this.render('index.ect', {
             question: data.question,
             title: 'Matching',
